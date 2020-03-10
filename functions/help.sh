finnix/live-build

commit d0eb72a5efd0b1cdbd22f3b5afa3cf16a731b482

help/usage: avoid unnecessary use of echo helpers
Closes: #952877

functions/help.sh
@@ -10,27 +10,22 @@
 
 Help ()
 {
-	Echo "%s - %s" "${PROGRAM}" "${DESCRIPTION}"
-	echo
-	Echo "Usage:"
-	echo
+	printf "%s - %s\n\n" "${PROGRAM}" "${DESCRIPTION}"
+	printf "Usage:\n\n"
 
 	if [ -n "${USAGE}" ]
 	then
-		Echo "${USAGE}"
-		echo
+		printf "%s\n" "${USAGE}"
 	fi
-	Echo "  %s [-h|--help]" "${PROGRAM}"
-	Echo "  %s [-u|--usage]" "${PROGRAM}"
-	Echo "  %s [-v|--version]" "${PROGRAM}"
-	echo
+	printf "  %s [-h|--help]\n" "${PROGRAM}"
+	printf "  %s [-u|--usage]\n" "${PROGRAM}"
+	printf "  %s [-v|--version]\n\n" "${PROGRAM}"
 
 	if [ -n "${HELP}" ]
 	then
-		Echo "${HELP}"
-		echo
+		printf "%s\n\n" "${HELP}"
 	fi
 
-	Echo "Report bugs to the Live Systems project <https://wiki.debian.org/DebianLive>."
+	echo "Report bugs to the Live Systems project <https://wiki.debian.org/DebianLive>."
 	exit 0
 }

functions/usage.sh
@@ -10,22 +10,19 @@
 
 Usage ()
 {
-	printf "%s - %s\n" "${PROGRAM}" "${DESCRIPTION}"
-	echo
-	Echo "Usage:"
-	echo
+	printf "%s - %s\n\n" "${PROGRAM}" "${DESCRIPTION}"
+	printf "Usage:\n\n"
 
 	if [ -n "${USAGE}" ]
 	then
-		Echo " ${USAGE}"
-		echo
+		printf "  %s\n" "${USAGE}"
 	fi
 
 	printf "  %s [-h|--help]\n" "${PROGRAM}"
 	printf "  %s [-u|--usage]\n" "${PROGRAM}"
 	printf "  %s [-v|--version]\n" "${PROGRAM}"
-	echo
-	Echo "Try \"%s --help\" for more information." "${PROGRAM}"
+
+	printf "\nTry \"%s --help\" for more information.\n" "${PROGRAM}"
 
 	exit 1
 }

commit bc199a73b68b9b31327afbb1c32ff82661bcccf5

help/usage: fix output of `lb config --usage`
broken by d0eb72a5efd0b1cdbd22f3b5afa3cf16a731b482

also, needed spaces before per-script output

Gbp-Dch: Ignore

functions/help.sh
@@ -15,7 +15,8 @@ Help ()
 
 	if [ -n "${USAGE}" ]
 	then
-		printf "%s\n" "${USAGE}"
+		# printf without placeholder required here for correct \t and \n formatting of `lb config --usage`
+		printf "  ${USAGE}\n"
 	fi
 	printf "  %s [-h|--help]\n" "${PROGRAM}"
 	printf "  %s [-u|--usage]\n" "${PROGRAM}"

functions/usage.sh
@@ -15,7 +15,8 @@ Usage ()
 
 	if [ -n "${USAGE}" ]
 	then
-		printf "  %s\n" "${USAGE}"
+		# printf without placeholder required here for correct \t and \n formatting of `lb config --usage`
+		printf "  ${USAGE}\n"
 	fi
 
 	printf "  %s [-h|--help]\n" "${PROGRAM}"