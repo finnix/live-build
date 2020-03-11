finnix/live-build

commit 8321653cb36511324d576e65cb13b5c9b0c5f438

Adding check to detect gnu-fdisk and using the original fdisk, thanks to Peter Skogström <peter.skogstrom@bitrunner.com> for the report.

functions/defaults.sh
@@ -138,10 +138,27 @@ Set_defaults ()
 		fi
 	fi
 
+	# Setting fdisk
+	if [ -z "${LH_FDISK}" ] || [ ! -x "${LH_FDISK}" ]
+	then
+		# Workaround for gnu-fdisk divertion
+		# (gnu-fdisk is buggy, #445304).
+		if [ -x /sbin/fdisk.distrib ]
+		then
+			LH_FDISK="fdisk.distrib"
+		elif [ -x /sbin/fdisk ]
+		then
+			LH_FDISK="fdisk"
+		else
+			echo "E: Can't proces file /sbin/fdisk (FIXME)"
+		fi
+	fi
+
 	# Setting losetup
 	if [ -z "${LH_LOSETUP}" ] || [ ! -x "${LH_LOSETUP}" ]
 	then
 		# Workaround for loop-aes-utils divertion
+		# (loop-aes-utils' losetup lacks features).
 		if [ -x /sbin/losetup.orig ]
 		then
 			LH_LOSETUP="losetup.orig"

helpers/lh_config
@@ -53,6 +53,7 @@ USAGE="${PROGRAM} [--apt apt|aptitude]\n\
 \t    [--debug]\n\
 \t    [-d|--distribution CODENAME]\n\
 \t    [-e|--encryption aes128|aes192|aes256]\n\
+\t    [--fdisk\n\
 \t    [--force]\n\
 \t    [--genisoimage genisomage|mkisofs]\n\
 \t    [--grub-splash FILE]\n\
@@ -108,7 +109,7 @@ USAGE="${PROGRAM} [--apt apt|aptitude]\n\
 
 Local_arguments ()
 {
-	ARGUMENTS="$(getopt --longoptions apt:,apt-ftp-proxy:,apt-http-proxy:,apt-pdiffs:,apt-pipeline:,apt-recommends:,apt-secure:,bootstrap:,cache:,cache-indices:,cache-packages:,cache-stages:,debconf-frontend:,debconf-nowarnings:,debconf-priority:,genisoimage:,initramfs:,losetup:,mode:,root-command:,use-fakeroot:,tasksel:,includes:,templates:,architecture:,bootstrap-config:,bootstrap-flavour:,bootstrap-keyring:,distribution:,mirror-bootstrap:,mirror-bootstrap-security:,mirror-binary:,mirror-binary-security:,sections:,chroot-filesystem:,union-filesystem:,exposed-root:,hooks:,interactive:,keyring-packages:,language:,linux-flavours:,linux-packages:,packages:,packages-lists:,tasks:,security:,symlinks:,sysvinit:,binary-images:,binary-indices:,bootappend-install:,bootappend-live:,bootloader:,chroot-build:,debian-installer:,debian-installer-daily:,encryption:,grub-splash:,hostname:,iso-application:,iso-preparer:,iso-publisher:,iso-volume:,memtest:,net-root-filesystem:,net-root-mountoptions:,net-root-path:,net-root-server:,net-cow-filesystem:,net-cow-mountoptions:,net-cow-path:,net-cow-server:,syslinux-splash:,syslinux-timeout:,syslinux-menu:,username:,source:,source-images:,breakpoints,conffile:,debug,force,help,quiet,usage,verbose,version --name=${PROGRAM} --options a:f:d:m:l:k:p:b:e:s:c:huv --shell sh -- "${@}")"
+	ARGUMENTS="$(getopt --longoptions apt:,apt-ftp-proxy:,apt-http-proxy:,apt-pdiffs:,apt-pipeline:,apt-recommends:,apt-secure:,bootstrap:,cache:,cache-indices:,cache-packages:,cache-stages:,debconf-frontend:,debconf-nowarnings:,debconf-priority:,genisoimage:,initramfs:,fdisk:,losetup:,mode:,root-command:,use-fakeroot:,tasksel:,includes:,templates:,architecture:,bootstrap-config:,bootstrap-flavour:,bootstrap-keyring:,distribution:,mirror-bootstrap:,mirror-bootstrap-security:,mirror-binary:,mirror-binary-security:,sections:,chroot-filesystem:,union-filesystem:,exposed-root:,hooks:,interactive:,keyring-packages:,language:,linux-flavours:,linux-packages:,packages:,packages-lists:,tasks:,security:,symlinks:,sysvinit:,binary-images:,binary-indices:,bootappend-install:,bootappend-live:,bootloader:,chroot-build:,debian-installer:,debian-installer-daily:,encryption:,grub-splash:,hostname:,iso-application:,iso-preparer:,iso-publisher:,iso-volume:,memtest:,net-root-filesystem:,net-root-mountoptions:,net-root-path:,net-root-server:,net-cow-filesystem:,net-cow-mountoptions:,net-cow-path:,net-cow-server:,syslinux-splash:,syslinux-timeout:,syslinux-menu:,username:,source:,source-images:,breakpoints,conffile:,debug,force,help,quiet,usage,verbose,version --name=${PROGRAM} --options a:f:d:m:l:k:p:b:e:s:c:huv --shell sh -- "${@}")"
 
 	if [ "${?}" != "0" ]
 	then
@@ -199,6 +200,11 @@ Local_arguments ()
 
 			--initramfs)
 				LH_INITRAMFS="${2}"
+				shift 2
+				;;
+
+			--fdisk)
+				LH_FDISK="${2}"
 				shift 2
 				;;
 
@@ -698,6 +704,10 @@ LH_DEBCONF_PRIORITY="${LH_DEBCONF_PRIORITY}"
 # \$LH_INITRAMFS: set initramfs hook
 # (Default: ${LH_INITRAMFS})
 LH_INITRAMFS="${LH_INITRAMFS}"
+
+# \$LH_FDISK: set fdisk program
+# (Default: autodetected)
+LH_FDISK="${LH_FDISK}"
 
 # \$LH_LOSETUP: set losetup program
 # (Default: autodetected)

helpers/lh_losetup
@@ -38,7 +38,7 @@ FILE="${2}"
 PARTITION="${3:=1}"
 
 ${LH_ROOT_COMMAND} ${LH_LOSETUP} "${DEVICE}" "${FILE}"
-FDISK_OUT="$(fdisk -l -u ${DEVICE} 2>&1)"
+FDISK_OUT="$(LH_FDISK -l -u ${DEVICE} 2>&1)"
 ${LH_ROOT_COMMAND} ${LH_LOSETUP} -d "${DEVICE}"
 
 LOOPDEVICE="$(echo ${DEVICE}p${PARTITION})"

commit d9f353c7370c34cf9e1a68cee544550678468535

remove obsolete fdisk hack
8321653cb36511324d576e65cb13b5c9b0c5f438 (from 2007) introduced a hack to
work around bug #445304 in gnu-fdisk for users who may have replaced fdisk
with the classic gnu version. the hack allowed users to select an alternate
fdisk binary to use to work around the buggy binary.

bug #445304 is marked as found in v1.0-1 and fixed in v1.2-1, though may
have been fixe din v1.1. it was marked fixed in 2009.

checking the package archive, gnu-fdisk does not actually exist anymore
in debian, with one exception - it is available for arm64 on sid via
debports, and that version is 1.3 so thus includes the necessary fix
anyway.

it is thus pointless now that we still carry this hack.

Gbp-Dch: Short

functions/defaults.sh
@@ -273,22 +273,6 @@ Set_config_defaults ()
 			;;
 	esac
 
-	# Setting fdisk
-	if [ -z "${LB_FDISK}" ] || [ ! -x "${LB_FDISK}" ]
-	then
-		# Workaround for gnu-fdisk divertion
-		# (gnu-fdisk is buggy, #445304).
-		if [ -x /sbin/fdisk.distrib ]
-		then
-			LB_FDISK="fdisk.distrib"
-		elif [ -x /sbin/fdisk ]
-		then
-			LB_FDISK="fdisk"
-		else
-			Echo_error "Can't process file /sbin/fdisk"
-		fi
-	fi
-
 	# Setting losetup
 	if [ -z "${LB_LOSETUP}" ] || [ "${LB_LOSETUP}" != "/sbin/losetup.orig" ]
 	then

functions/losetup.sh
@@ -42,7 +42,7 @@ Losetup ()
 	PARTITION="${3:-1}"
 
 	${LB_LOSETUP} --read-only --partscan "${DEVICE}" "${FILE}"
-	FDISK_OUT="$(${LB_FDISK} -l -u ${DEVICE} 2>&1)"
+	FDISK_OUT="$(fdisk -l -u ${DEVICE} 2>&1)"
 	Lodetach "${DEVICE}"
 
 	LOOPDEVICE="$(echo ${DEVICE}p${PARTITION})"

manpages/en/lb_config.1
@@ -104,8 +104,6 @@
 .br
 	[\fB\-\-dump\fR]
 .br
-	[\fB\-\-fdisk\fR fdisk|fdisk.dist]
-.br
 	[\fB\-\-firmware\-binary\fR true|false]
 .br
 	[\fB\-\-firmware\-chroot\fR true|false]
@@ -349,8 +347,6 @@ defines the distribution enabled in the resulting live system (defaults to the v
 defines the distribution used to build the live system (defaults to the value set in \fB\-\-distribution\fR)
 .IP "\fB\-\-dump\fR" 4
 prepares a report of the currently present live system configuration and the version of live\-build used. This is useful to provide if you submit bug reports, we do get all information required for us to locate and replicate an error.
-.IP "\fB\-\-fdisk\fR fdisk|fdisk.dist" 4
-sets the filename of the fdisk binary from the host system that should be used. This is autodetected and does generally not need any customization.
 .IP "\fB\-\-firmware\-binary\fR true|false" 4
 includes firmware packages in debian-installer. Defaults to true.
 .IP "\fB\-\-firmware\-chroot\fR true|false" 4

scripts/build/binary_hdd
@@ -53,6 +53,7 @@ esac
 
 Check_package chroot /usr/share/doc/mtools mtools
 Check_package chroot /sbin/parted parted
+Check_package host /sbin/fdisk fdisk
 
 echo "${LB_BOOTLOADERS}" | \
 while IFS="," read -r BOOTLOADER

scripts/build/config
@@ -65,7 +65,6 @@ USAGE="${PROGRAM}   [--apt apt|aptitude]\n\
 \t    [--distribution-binary CODENAME]\n\
 \t    [--distribution-chroot CODENAME]\n\
 \t    [--dump]\n\
-\t    [--fdisk fdisk|fdisk.dist]\n\
 \t    [--firmware-binary true|false]\n\
 \t    [--firmware-chroot true|false]\n\
 \t    [--force]\n\
@@ -141,7 +140,7 @@ Local_arguments ()
 	LONG_OPTIONS="apt:,apt-ftp-proxy:,apt-http-proxy:,apt-options:,aptitude-options:,debootstrap-options:,debootstrap-script:,
 		apt-pipeline:,apt-recommends:,apt-secure:,apt-source-archives:,cache:,cache-indices:,cache-packages:,
 		cache-stages:,debconf-frontend:,debconf-priority:,dump,
-		initramfs:,initramfs-compression:,initsystem:,fdisk:,losetup:,mode:,system:,tasksel:,
+		initramfs:,initramfs-compression:,initsystem:,losetup:,mode:,system:,tasksel:,
 		architectures:,clean,
 		distribution:,parent-distribution:,parent-debian-installer-distribution:,
 		distribution-chroot:,parent-distribution-chroot:,
@@ -330,7 +329,7 @@ Local_arguments ()
 				;;
 
 			--fdisk)
-				LB_FDISK="${2}"
+				Echo_warning "--fdisk is an obsolete option"
 				shift 2
 				;;
 
@@ -1010,10 +1009,6 @@ LB_INITRAMFS_COMPRESSION="${LB_INITRAMFS_COMPRESSION}"
 # \$LB_INITSYSTEM: set init system
 # (Default: ${LB_INITSYSTEM})
 LB_INITSYSTEM="${LB_INITSYSTEM}"
-
-# \$LB_FDISK: set fdisk program
-# (Default: autodetected)
-LB_FDISK="${LB_FDISK}"
 
 # \$LB_LOSETUP: set losetup program
 # (Default: autodetected)

scripts/build/source_hdd
@@ -48,6 +48,7 @@ Create_lockfile .lock
 # Checking depends
 Check_package chroot /sbin/mkdosfs dosfstools
 Check_package chroot /sbin/parted parted
+Check_package host /sbin/fdisk fdisk
 
 # Installing depends
 Install_package