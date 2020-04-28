61aa3239c5c93d6d7bdc5b9b6a407f400799779b
Lyndon Brown <jnqnfe@gmail.com>
2020-04-28 18:32:36 +0100

    config: remove junk config loading code
    support for this has not existed since v2.0~a24-1 !!
    
    until 4d22ca948ad3def3f98fd89d32bf2ba8209460fd (v2.0~a24-1), `PROGRAM` was
    defined as `PROGRAM="$(basename ${0})"`. then this commit simply got rid
    of the definition.
    
    it then flipped back and forth a bit in a few ways, but always sticking
    to a simple fixed "live-build" type string after that commit, as it has
    been ever since.
    
    the code removed here has thus been junk since then, and the documentation
    corrected here out of date since then.
    
    the checking of `LB_CONFIG` is also possibly junk, but i know too little
    about it. it was introduced in v1.0~a43-1 in commit
    705a4178e75ffc190acf2fa914a6cd1519fdae87 with a changelog entry of
    "Centraly evaluate LH_CONFIG for custom configuration file."
    
    Gbp-Dch: Short

--- functions/conffile.sh
+++ functions/conffile.sh
@@ -19,9 +19,6 @@ Get_conffiles ()
 		local FILE
 		for FILE in "${@}"; do
 			FILES="${FILES} ${FILE} ${FILE}.${LB_ARCHITECTURE} ${FILE}.${DISTRIBUTION}"
-			FILES="${FILES} config/$(echo ${PROGRAM} | sed -e 's|^lb_||')"
-			FILES="${FILES} config/$(echo ${PROGRAM} | sed -e 's|^lb_||').${ARCHITECTURE}"
-			FILES="${FILES} config/$(echo ${PROGRAM} | sed -e 's|^lb_||').${DISTRIBUTION}"
 		done
 	fi
 
