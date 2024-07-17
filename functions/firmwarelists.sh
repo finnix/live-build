22c48e92be0bc1c5cffb0636ab0db29fe108a969
Roland Clobus <rclobus@rclobus.nl>
2024-07-17 20:31:48 +0200

    Fixed: select the package name for firmware packages
    When the firmware package contains file names with spaces (e.g.
    firmware-brcm80211, 20240610-1), pick the correct column for the package
    section

--- functions/firmwarelists.sh
+++ functions/firmwarelists.sh
@@ -50,7 +50,7 @@ Firmware_List_From_Contents () {
 			fi
 
 			local PACKAGES
-			PACKAGES="$(gunzip -c "${CONTENTS_FILE}" | awk '/^(usr\/)?lib\/firmware/ { print $2 }' | sort -u )"
+			PACKAGES="$(gunzip -c "${CONTENTS_FILE}" | awk '/^(usr\/)?lib\/firmware/ { print $NF }' | sort -u )"
 			FIRMWARE_PACKAGES="${FIRMWARE_PACKAGES} ${PACKAGES}"
 
 			# Don't waste disk space, if not making use of caching
